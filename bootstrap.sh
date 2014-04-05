34d36f925a6ad5da65d4dc4bda737e519646ecde
Paul Herron <contact@paulherron.net>
2014-04-05 11:49:50 +0100

    Remove xdebug for performance reasons.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ apt-get update
 
 debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password 100rows'
 debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password 100rows'
-apt-get install -qy nginx php5 php5-fpm php5-gd php5-mysql php5-cgi php5-cli php5-curl ffmpeg vim git-core mysql-server-5.5 mysql-client curl php5-xdebug tmux
+apt-get install -qy nginx php5 php5-fpm php5-gd php5-mysql php5-cgi php5-cli php5-curl ffmpeg vim git-core mysql-server-5.5 mysql-client curl tmux
 
 echo "Moving nginx config files into place…"
 rm /etc/nginx/sites-enabled/default
@@ -22,8 +22,6 @@ mysql -u root -p100rows < /vagrant/mysql/database.sql
 echo "Moving php config files into place…"
 mv /etc/php5/fpm/php.ini /etc/php5/fpm/php.ini.default
 cp /vagrant/php/php.ini /etc/php5/fpm/php.ini
-rm /etc/php5/fpm/conf.d/xdebug.ini
-cp /vagrant/php/xdebug.ini /etc/php5/fpm/conf.d/
 
 echo "Starting services…"
 service nginx restart
