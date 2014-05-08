25a5c93fea4c37e7faf6b0234c18efc210abd0c1
Paul Herron <contact@paulherron.net>
2014-05-08 12:44:01 +0100

    Use UTF8 for database by default.

--- mysql/database.sql
+++ mysql/database.sql
@@ -1,2 +1,2 @@
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%'  IDENTIFIED  BY '100rows';
-create database `default`;
+CREATE DATABASE `default` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci
