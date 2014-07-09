4b9ddb6e1f0ea6fbae74a8787a87fad975155e68
Anu Gupta <anu@digitalquery.com>
2014-07-09 16:35:16 +0100

    root mysql password

--- provisioning/mysql/database.sh
+++ provisioning/mysql/database.sh
@@ -2,11 +2,11 @@
 
 
 echo "*** deploying database dump"
-mysql -u root -proot -e "CREATE DATABASE IF NOT EXISTS $wp_db_name;"
-mysql -u root -proot $wp_db_name < /vagrant/provisioning/$wp_db_dump_file
+mysql -u root -p$mysql_root_password -e "CREATE DATABASE IF NOT EXISTS $wp_db_name;"
+mysql -u root -p$mysql_root_password $wp_db_name < /vagrant/provisioning/$wp_db_dump_file
 
 if [ ! -z "$wp_db_user" ]; then
-  mysql -u root -proot -e "GRANT ALL ON $wp_db_name.* TO '$wp_db_user'@'localhost' IDENTIFIED BY '$wp_db_password'"
+  mysql -u root -p$mysql_root_password -e "GRANT ALL ON $wp_db_name.* TO '$wp_db_user'@'localhost' IDENTIFIED BY '$wp_db_password'"
 fi
 
 echo "*** wp-cli search and replace"
