46a130e052df25b9c38037c8ea0e36b8e5f08f08
Anu Gupta <anu@digitalquery.com>
2014-07-09 09:45:39 +0100

    Adding config options for database username and password and more explanation of options

--- provisioning/settings.sh
+++ provisioning/settings.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
 
-db_name='ds_blogs'
-db_dump_file='ds_blogs.sql'
-import_site_domain='digital-science.dev'
+# name of database: eg wordpress
+db_name=''
+
+# database user name
+db_user=''
+
+# database password
+db_password=''
+
+# set to true to import the database as part of the provisioning process
 deploy_database=true
-wordpress_path='/vagrant/blog'
+
+# database dump file that should be imported automatically: eg wordpress_dump.sql
+db_dump_file=''
+
+# specify the domain that the imported dump file uses.
+# This allows us to search and replace the domain with nginx.local
+# eg: mysite.com
+import_site_domain=''
+
+# specify path (inside the Vagrant VM) to WordPress to allow wp-cli to do search and replace
+# eg /vagrant
+wordpress_path=''
