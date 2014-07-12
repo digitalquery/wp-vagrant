4442d55030339b371a1ab7b2eeb6837f4be89ac5
Anu Gupta <anu@digitalquery.com>
2014-07-12 17:23:40 +0100

    better initial database script renamed $deploy_database to $import_database

--- provisioning/mysql/database.sh
+++ provisioning/mysql/database.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-# if $wp_db_name is specified, then create the database
+# if $wp_db_name is specified, then create the database and user (if neccesary)
+# if import_database is specified then import database and use wp-cli to rename domain
 if $wp_db_name ; then
-
 
   echo "**** creating database"
   mysql -u root -p$mysql_root_password -e "CREATE DATABASE IF NOT EXISTS $wp_db_name;"
-
 
   if [ ! -z "$wp_db_user" ]; then
     mysql -u root -p$mysql_root_password -e "GRANT ALL ON $wp_db_name.* TO '$wp_db_user'@'localhost' IDENTIFIED BY '$wp_db_password'"
