76e39f603514636c941da5c41899f5309e9253b2
Anu Gupta <anu@digitalquery.com>
2014-07-15 00:03:04 +0100

    Don't try to copy removed dummy SSL files

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -4,5 +4,3 @@ if [ -f /etc/nginx/sites-enabled/default ]; then
   rm /etc/nginx/sites-enabled/default
 fi
 cp /vagrant/wp-vagrant/nginx/default.conf /etc/nginx/sites-enabled/
-# cp /vagrant/wp-vagrant/nginx/fastcgi_params.conf /etc/nginx/
-cp /vagrant/wp-vagrant/nginx/dummy.* /etc/nginx/
