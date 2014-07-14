8b6b1d6a91497975daa603d66b75c6aaf0fba91f
Anu Gupta <anu@digitalquery.com>
2014-07-14 11:30:32 +0100

    Fix database dump script

--- provisioning/mysql/db_dump.sh
+++ provisioning/mysql/db_dump.sh
@@ -5,23 +5,24 @@
 . /vagrant/wp-vagrant-settings.sh
 
 
-if $wp_db_name ; then
+echo "*****db name is $wp_db_name"
+
+
+if [ ! -z $wp_db_name ]; then
 
   now=`date +"%Y_%m_%d-%H-%M-%S"`
 
+  db_dump_file=$wp_db_name"_"$now".sql"
 
   echo "**** dumping database before destroy"
-  echo "**** dump file: /vagrant/provision/db_dumps/$wp_db_name_$now.sql"
+  echo "**** dump file: /vagrant/provision/db_dumps/$db_dump_file"
 
 
   if [ ! -d /vagrant/provisioning/db_dumps ]; then
     mkdir /vagrant/provisioning/db_dumps
   fi
 
-
-    echo "dumping $wp_db_name with $mysql_root_password"
-
-    mysqldump -u root --password=$mysql_root_password $wp_db_name > /vagrant/provisioning/db_dumps/$wp_db_name_$now.sql
+  mysqldump -u root --password=$mysql_root_password $wp_db_name > /vagrant/provisioning/db_dumps/$db_dump_file
 
   if [ ! "$?" -eq 0 ]; then
     echo "***** DATABASE DUMP FAILED - YOU MAY WISH TO ABORT VAGRANT DESTROY."
