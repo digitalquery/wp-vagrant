9c9ab34d353acaab94300cefc1b97c8452c38414
Anu <anu@digitalquery.com>
2014-11-09 19:14:14 +0000

    Don't copy nginx site config into sites-enabled, instead copy it to sites-available and create symlink in sites-enabled

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+# copy nginx config
 cp /vagrant/wp-vagrant/nginx/nginx.conf /etc/nginx/
 
+# remove default site
 if [ -f /etc/nginx/sites-enabled/default ]; then
   rm /etc/nginx/sites-enabled/default
 fi
-cp /vagrant/wp-vagrant/nginx/default.conf /etc/nginx/sites-enabled/
+
+# copy our site config and synlink it
+cp /vagrant/wp-vagrant/nginx/default.conf /etc/nginx/sites-available/
+ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enables/default.conf
