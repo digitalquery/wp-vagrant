3f30de445f69fb6d101190531782fafa54c84f41
Anu Gupta <anu@digitalquery.com>
2015-09-04 13:57:37 +0100

    remove nginx's default.conf (added in the base box) - this will make sure that whatever new domain name is chosen will work

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -7,6 +7,10 @@ cp /vagrant/wp-vagrant/nginx/nginx.conf /etc/nginx/
 if [ -f /etc/nginx/sites-enabled/default ]; then
   rm /etc/nginx/sites-enabled/default
 fi
+if [ -f /etc/nginx/sites-enabled/default.conf ]; then
+  rm /etc/nginx/sites-enabled/default.conf
+fi
+
 
 # copy our site config and synlink it
 cp /vagrant/wp-vagrant/nginx/default.conf /etc/nginx/sites-available/
