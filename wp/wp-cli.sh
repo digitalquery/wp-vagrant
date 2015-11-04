f46930b9839fc932d65427760134fc1197e032c8
Anu Gupta <anu@digitalquery.com>
2015-11-04 13:14:29 +0000

    correctly creates folder in /home/vagrant

--- wp/wp-cli.sh
+++ wp/wp-cli.sh
@@ -6,8 +6,11 @@ if [ ! -f /usr/local/bin/wp ]; then
   curl  https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli-nightly.phar -o /tmp/wp-cli.phar
   chmod +x /tmp/wp-cli.phar
   sudo mv /tmp/wp-cli.phar /usr/local/bin/wp
+fi
 
-  # default wp-cli config
-  cp /vagrant/wp-vagrant/wp/wp-cli.config.yml ~/.wp-cli/config.yml
-
+wpcli_defaults_folder='/home/vagrant/.wp-cli'
+if [ ! -d $wpcli_defaults_folder  ]; then
+  echo "**** adding wp-cli defaults"
+  mkdir $wpcli_defaults_folder
+  cp /vagrant/wp-vagrant/wp/wp-cli.config.yml $wpcli_defaults_folder/config.yml
 fi
